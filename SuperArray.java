aee9cdb60672d638ba0aa2afab934ef1e99113a2
hongweimachene <hchen00@stuy.edu>
2018-10-25 09:51:35 -0400

    Superarray

--- SuperArray.java
+++ SuperArray.java
@@ -22,31 +22,55 @@ public class SuperArray {
   }
 
   public boolean add(String s) {
+    if (size == data.length){
+      resize();
+    }
     data[size] = s;
     size+=1;
     return true;
   }
 
   public String toString() {
+    String s = "[";
+    for (int i = 0; i < size; i++) {
+      if (i == size - 1) {
+        s += data[i] + "]";
+      } else {
+        s += data[i] + ",";
+      }
+    }
+    return s;
+  }
 
+  public String toStringDebug() {
+    String s = "[";
+    for (int i = 0; i < data.length; i++) {
+      if (i == size - 1) {
+        s += data[i] + "]";
+      } else {
+        s += data[i] + ",";
+      }
+    }
+    return s;
   }
 
   public String get(int index) {
     if (index < 0 || index >= size()) {
       return null;
-    } else {
-      return ""+data[index];
     }
+    return data[index];
   }
 
-  public String set(int index) {
+  public String set(int index, String s) {
     if (index < 0 || index >= size()) {
       return null
     }
+    data[index] = s;
+    return data[index];
   }
 
   private void resize() {
-    String [] fill = new String[size * 2 + 1];
+    String[] fill = new String[size * 2 + 1];
     for (int i = 0; i < data.length; i++) {
       fill[i] = data[i];
     }
